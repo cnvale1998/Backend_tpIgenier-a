9fc304c6f06e71542425a9de17d0da70f2fa5afd
pedrok <pedroltr89@gmail.com>
2019-06-18 19:24:04 -0300

    se agrego funcionalidades para CU vERhORARIOS

--- src/controllers/TransmitenControllers.ts
+++ src/controllers/TransmitenControllers.ts
@@ -6,14 +6,19 @@ class TransmitenControllers{
 
   
   public async verTransmision(req: Request, res: Response): Promise<any> {
-    const { idPelicula } = req.params;
+    const { id_pelicula } = req.params;
 
-        const transmision = await pool.query("SELECT * FROM transmiten WHERE ID_PELICULA = ? ",[idPelicula]);
-        if(transmision.length>0){
+        const transmision = await pool.query("SELECT * FROM transmiten WHERE ID_PELICULA = ?; ",[id_pelicula]);
+        console.log(req.params);
+        //CON ESTO EVITAMOS QUE NOS DEVUELVA UN ARREGLO CON 1 SOLO ELEMENTO [{ELEMENTO}]
+        //Y ASEGURAMOS QUE DEVUELVA {ELEMENTO} QUE NO ES ARREGLO EQUIVALENTE A LA INTERFACE USADA.
+        if(transmision.length >0){
           return res.json(transmision[0]);
         }
-
+        
         res.status(404).json({text: "no hay transmisiones para esa pelicula"});
+
+       
       
 }
 
