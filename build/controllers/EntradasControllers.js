9ba9105c50c3ff00d3cde32b816e6b872a155a17
manuelnc53 <manuelnc53@gmail.com>
2019-07-01 03:50:01 -0300

    email

--- build/controllers/EntradasControllers.js
+++ build/controllers/EntradasControllers.js
@@ -15,10 +15,10 @@ const database_1 = __importDefault(require("../database"));
 class EntradasControllers {
     put(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            var valores = { ID_PELICULA: Number, ID_BENEFICIO: Number, PRECIO: Number, FECHA: Date, TOTAL: Number, ID_COMBO: Number };
+            var valores = { ID_PELICULA: Number, ID_BENEFICIO: Number, PRECIO: Number, FECHA: Date, TOTAL: Number, ID_COMBO: Number, EMAIL: String };
             valores = req.body;
             try {
-                var ticket = yield database_1.default.query('INSERT INTO TICKETS(TOTAL, ID_COMBO) VALUES (?,?)', [valores.TOTAL, valores.ID_COMBO]);
+                var ticket = yield database_1.default.query('INSERT INTO TICKETS(TOTAL, ID_COMBO, EMAIL) VALUES (?,?,?)', [valores.TOTAL, valores.ID_COMBO, valores.EMAIL]);
                 var query_ticket = yield database_1.default.query('SELECT MAX(ID_TICKET) AS id FROM TICKETS');
                 var id_ticket = JSON.parse(JSON.stringify(query_ticket));
                 const result = yield database_1.default.query('INSERT INTO ENTRADAS(ID_PELICULA,PRECIO,ID_BENEFICIO, ID_TICKET, FECHA) VALUES  (?,?,?,?,?)', [valores.ID_PELICULA, valores.PRECIO, valores.ID_BENEFICIO, id_ticket[0].id, valores.FECHA]);
--- src/controllers/EntradasControllers.ts
+++ src/controllers/EntradasControllers.ts
@@ -5,11 +5,11 @@ class EntradasControllers{
 
 
   public async put (req: Request,res: Response): Promise<void> {
-    var  valores={ID_PELICULA:Number,ID_BENEFICIO: Number, PRECIO: Number, FECHA:Date, TOTAL:Number, ID_COMBO:Number }; 
+    var  valores={ID_PELICULA:Number,ID_BENEFICIO: Number, PRECIO: Number, FECHA:Date, TOTAL:Number, ID_COMBO:Number, EMAIL: String }; 
     valores= req.body;
     
         try{
-          var ticket=await pool.query('INSERT INTO TICKETS(TOTAL, ID_COMBO) VALUES (?,?)',[valores.TOTAL,valores.ID_COMBO]);
+          var ticket=await pool.query('INSERT INTO TICKETS(TOTAL, ID_COMBO, EMAIL) VALUES (?,?,?)',[valores.TOTAL,valores.ID_COMBO, valores.EMAIL]);
           var query_ticket = await pool.query('SELECT MAX(ID_TICKET) AS id FROM TICKETS');
           var id_ticket = JSON.parse(JSON.stringify(query_ticket));
           const result = await pool.query('INSERT INTO ENTRADAS(ID_PELICULA,PRECIO,ID_BENEFICIO, ID_TICKET, FECHA) VALUES  (?,?,?,?,?)', [valores.ID_PELICULA,valores.PRECIO,valores.ID_BENEFICIO,id_ticket[0].id,valores.FECHA]);
