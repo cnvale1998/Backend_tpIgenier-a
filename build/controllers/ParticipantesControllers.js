df0049064cda4566727f552785cb361004c8de41
cnvale1998 <cnvale1998@gmail.com>
2019-07-05 10:38:54 -0300

    cambios CU de leo para que no tire error

--- build/controllers/ParticipantesControllers.js
+++ build/controllers/ParticipantesControllers.js
@@ -13,20 +13,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("../database"));
 class ParticipantesControllers {
+    /*public async getParticipante(req: Request, res: Response): Promise<void> {
+      const participadores = await pool.query('SELECT participantes.EMAIL FROM participantes');
+      res.json(participadores);
+     
+   }*/
     getParticipante(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            const participadores = yield database_1.default.query('SELECT participantes.EMAIL FROM participantes');
-            res.json(participadores);
+            const { email } = req.params;
+            const resultado = yield database_1.default.query("SELECT COUNT(EMAIL) FROM PARTICIPANTES WHERE EMAIL= ?", [email]);
+            console.log(resultado);
+            res.json(resultado);
         });
     }
     insertarParticipante(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 const result = yield database_1.default.query('INSERT INTO participantes set ?', [req.body]);
-                req.body.json({});
+                console.log("inserto participante");
+                //req.body.json({});
                 res.json({ message: 'se ha guardado el participante' });
             }
             catch (e) {
+                console.log(e);
                 res.json({ message: 'ocurrio un error lala' });
             }
         });
--- src/controllers/ParticipantesControllers.ts
+++ src/controllers/ParticipantesControllers.ts
@@ -4,10 +4,17 @@ import pool from '../database';
 class ParticipantesControllers{
 
 
- public async getParticipante(req: Request, res: Response): Promise<void> {
+ /*public async getParticipante(req: Request, res: Response): Promise<void> {
    const participadores = await pool.query('SELECT participantes.EMAIL FROM participantes');
    res.json(participadores);
   
+}*/
+
+public async getParticipante(req: Request, res: Response): Promise<void> {
+  const { email } = req.params;
+  const resultado = await pool.query("SELECT COUNT(EMAIL) FROM PARTICIPANTES WHERE EMAIL= ?",[email]);
+  console.log(resultado);
+  res.json(resultado);
 }
 
   public async insertarParticipante (req: Request,res: Response): Promise<void> {
@@ -15,7 +22,7 @@ class ParticipantesControllers{
         try{
           const result = await pool.query('INSERT INTO participantes set ?', [req.body]);
           console.log("inserto participante");
-          req.body.json({});
+          //req.body.json({});
           res.json({ message: 'se ha guardado el participante' });}
         catch(e){ 
           console.log(e);
